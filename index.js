e2aa5627bf962c34f5608c9f15613468220b9686
Your sivanandini <sivanandini2003@gmail.com>
2024-03-31 18:59:02 +0000

    React-projects

---
+++ index.js
@@ -0,0 +1,15 @@
+const element = (
+  <div className="container">
+    <h1>Supper Over League</h1>
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+      className="img"
+    />
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+      className="img"
+    />
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
